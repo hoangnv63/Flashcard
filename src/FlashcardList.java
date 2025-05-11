c4fbd42b13d0b6cc62eaad506a1c5ff35a4b9c39
hoangnv63 <vh2005dn7@gmail.com>
2025-05-11 15:03:36 +0700

    feat: create a Flashcard Linked list

---
+++ src/FlashcardList.java
@@ -0,0 +1,40 @@
+public class FlashcardList {
+    private FlashcardNode head;
+    private FlashcardNode tail;
+    private int size = 0;
+
+    public void add(Flashcard flashcard) {
+        FlashcardNode node = new FlashcardNode(flashcard);
+        if (head == null) {
+            head = tail = node;
+        } else {
+            tail.next = node;
+            node.prev = tail;
+            tail = node;
+        }
+        size++;
+    }
+
+    public FlashcardNode getHead() {
+        return head;
+    }
+
+    public FlashcardNode getTail() {
+        return tail;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int indexOf(FlashcardNode node) {
+        int index = 0;
+        FlashcardNode current = head;
+        while (current != null) {
+            if (current == node) return index;
+            current = current.next;
+            index++;
+        }
+        return -1;
+    }
+}
